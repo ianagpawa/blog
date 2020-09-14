736ab39f5ead2bde8f451aeb2d0ba29545d04c40
ian.agpawa <agpawaji@gmail.com>
2020-09-14 00:23:57 -0400

    update references

--- gatsby-config.js
+++ gatsby-config.js
@@ -79,8 +79,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Blogfolio`,
-        short_name: `Blogfolio`,
+        name: `BlogIt`,
+        short_name: `BlogIt`,
         start_url: `/`,
         background_color: `#f7f0eb`,
         theme_color: `#a2466c`,
@@ -88,6 +88,6 @@ module.exports = {
         icon: `static/assets/icon.png`,
       },
     },
-    'gatsby-plugin-offline',
+    // 'gatsby-plugin-offline',
   ],
 }
