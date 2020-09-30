de66dba5bad2b91c662d5555cb92c98bbc3456c6
ian.agpawa <agpawaji@gmail.com>
2020-09-29 23:56:28 -0400

    update with portfolio link

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -10,7 +10,7 @@ const ContactPage = ({
 }) => {
   return (
     <Layout>
-      <Helmet>
+      {/* <Helmet>
         <title>Contact — {site.siteMetadata.title}</title>
         <meta name="description" content={"Contact page of " + site.siteMetadata.description} />
       </Helmet>
@@ -42,7 +42,31 @@ const ContactPage = ({
             </div>
           </form>
         </div>
-      </div>
+      </div> */}
+      <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+  <input type="hidden" name="bot-field" />
+  <input type="hidden" name="form-name" value="contact" />
+  <div className="field half first">
+    <label htmlFor="name">Name</label>
+    <input type="text" name="name" id="name" />
+  </div>
+  <div className="field half">
+    <label htmlFor="email">Email</label>
+    <input type="text" name="email" id="email" />
+  </div>
+  <div className="field">
+    <label htmlFor="message">Message</label>
+    <textarea name="message" id="message" rows="6" />
+  </div>
+  <ul className="actions">
+    <li>
+      <input type="submit" value="Send Message" className="special" />
+    </li>
+    <li>
+      <input type="reset" value="Clear" />
+    </li>
+  </ul>
+</form>
     </Layout>
   )
 }
