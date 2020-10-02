5664018284687c671683ff26a4abf0af18ef9181
ian.agpawa <agpawaji@gmail.com>
2020-10-01 23:00:00 -0400

    update contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -10,30 +10,6 @@ const ContactPage = ({
 }) => {
   return (
     <Layout>
-      {/* <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
-        <input type="hidden" name="bot-field" />
-        <input type="hidden" name="form-name" value="contact" />
-        <div className="field half first">
-          <label htmlFor="name">Name</label>
-          <input type="text" name="name" id="name" />
-        </div>
-        <div className="field half">
-          <label htmlFor="email">Email</label>
-          <input type="text" name="email" id="email" />
-        </div>
-        <div className="field">
-          <label htmlFor="message">Message</label>
-          <textarea name="message" id="message" rows="6" />
-        </div>
-        <ul className="actions">
-          <li>
-            <input type="submit" value="Send Message" className="special" />
-          </li>
-          <li>
-            <input type="reset" value="Clear" />
-          </li>
-        </ul>
-      </form> */}
       <Helmet>
         <title>Contact — {site.siteMetadata.title}</title>
         <meta name="description" content={"Contact page of " + site.siteMetadata.description} />
@@ -47,50 +23,22 @@ const ContactPage = ({
         <form name="contact" className="form-container" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
           <input type="hidden" name="bot-field" />
           <input type="hidden" name="form-name" value="contact" />
-          <div className="field half first">
+          <div>
             <label htmlFor="name">Name</label>
             <input type="text" name="name" id="name" />
           </div>
-          <div className="field half">
+          <div>
             <label htmlFor="email">Email</label>
             <input type="text" name="email" id="email" />
           </div>
-          <div className="field">
+          <div>
             <label htmlFor="message">Message</label>
             <textarea name="message" id="message" rows="6" />
           </div>
-          <ul className="actions">
-            <li>
-              <input type="submit" value="Send Message" className="special" />
-            </li>
-            <li>
-              <input type="reset" value="Clear" />
-            </li>
-          </ul>
+          <div style={{display: "flex", justifyContent: "flex-end"}}>
+            <input type="submit" className="button -primary" style={{marginRight: 0}} />
+          </div>
         </form>
-          {/* <form name="contact" className="form-container" action="/thanks" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
-          <input type="hidden" name="bot-field" />
-          <input type="hidden" name="form-name" value="contact" />
-            <div>
-              <label htmlFor="name">Name</label>
-              <input type="text" name="name" id="w3lName"/>
-            </div>
-            <div>
-              <label htmlFor="email">Email</label>
-              <input type="email" name="email" id="w3lSender"/>
-            </div>
-            <div>
-              <label htmlFor="subject">Subject</label>
-              <input type="text" name="subject" id="w3lSubject"/>
-            </div>
-            <div>
-              <label htmlFor="message">Message</label>
-              <textarea name="w3lMesmessagesage" id="w3lMessage"></textarea>
-            </div>
-            <div style={{display: "flex", justifyContent: "flex-end"}}>
-              <input type="submit" className="button -primary" style={{marginRight: 0}} />
-            </div>
-          </form> */}
         </div>
       </div>
     </Layout>
